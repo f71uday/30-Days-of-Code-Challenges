6d2a448c9faa320e21cbeec1d9f3010e27a389fa
uday singh <f71uday@gmail.com>
2017-07-03 16:30:21 +0530

    final commit

---
+++ Class_vs_Instace.java
@@ -0,0 +1,43 @@
+import java.io.*;
+import java.util.*;
+public class Person {
+    private int age;
+    String msg;
+	public Person(int initialAge) {
+  		if (initialAge <0)
+        {     System.out.println("Age is not valid, setting age to 0.");
+               age=0;
+        }
+        else age =initialAge;
+	}
+
+	public void amIOld() {
+  		// Write code determining if this person's age is old and print the correct statement:
+        if (age<13)
+            msg="young.";
+        else if (age >=13&&age<18)
+            msg ="a teenager.";
+            else
+                msg = "old.";
+        System.out.println("You are "+msg);
+	}
+
+	public void yearPasses() {
+  		age = age +1;
+	}
+  public static void main(String[] args) {
+  		Scanner sc = new Scanner(System.in);
+  		int T = sc.nextInt();
+  		for (int i = 0; i < T; i++) {
+  			int age = sc.nextInt();
+  			Person p = new Person(age);
+  			p.amIOld();
+  			for (int j = 0; j < 3; j++) {
+  				p.yearPasses();
+  			}
+  			p.amIOld();
+  			System.out.println();
+          }
+  		sc.close();
+      }
+  }
